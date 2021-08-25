18ba355d48f117cf93d411046907a0063c866d57
shravan2524 <shravanchenna6@gmail.com>
2021-08-25 16:56:22 +0530

    react app setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from './Components/Nav';
+import Aboutus from './Components/Aboutus';
+import Container from './Components/Container';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Nav />
+          <Container />
+          <Aboutus />
       </header>
     </div>
   );
 }
 
 export default App;
+
+
+
+//Components
+// 1)Navbar
+// 2)CSI VIT heading
+// 3)About us & contact us
